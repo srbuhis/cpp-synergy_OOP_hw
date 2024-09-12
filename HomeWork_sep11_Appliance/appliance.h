9b3a1a877152e0ed18b3178b8b04f7e66fa9dfeb
srbuhis <160963114+srbuhis@users.noreply.github.com>
2024-09-12 13:18:14 +0400

    Update appliance.h

--- HomeWork_sep11_Appliance/appliance.h
+++ HomeWork_sep11_Appliance/appliance.h
@@ -1,1 +1,16 @@
+#ifndef APPLIANCE_H
+#define APPLIANCE_H
 
+class Appliance
+{
+public:
+	virtual void turnOn() = 0;
+	bool turnOff();
+	void setAction(bool &value);
+	bool getAction() const;
+	~Appliance() {};
+private:
+	bool action;	
+};
+
+#endif
